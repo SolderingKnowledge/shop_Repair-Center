e28bd7d7f5164e965177c89689137770970bd37b
Aikol <imaikol@yahoo.com>
2018-07-21 01:48:07 -0700

    Update views.py

--- Shop/src/SHOP/merchandise/views.py
+++ Shop/src/SHOP/merchandise/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Merchandise
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -10,8 +11,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def merchandise_list_view(request):                   #http://127.0.0.1:8000/merchandise_list_view/
     queryset = Merchandise.objects.all()
-    context = {'object_list': queryset}
+    context = {'merchandises': queryset}
+    query = request.GET.get("q")
+    if query:
+    	queryset = queryset.filter(
+                Q(title__icontains=query)
+            ).distinct()
+    	return render(request, 'list.html', context)
     return render(request, "list.html", context)
+
 
 
 
@@ -19,5 +27,8 @@ def merchandise_detail_view(request, id, *args, **kwargs):    #http://127.0.0.1:
 	instance = get_object_or_404(Merchandise, id=id)
 	context = {'object': instance}
 	return render(request, 'detail.html', context)
+
+
+
 
 

811d57e5df163d7493c9633c20e76225241e9a9c
Aikol <imaikol@yahoo.com>
2018-07-20 22:57:32 -0700

    Update views.py

--- Shop/src/SHOP/merchandise/views.py
+++ Shop/src/SHOP/merchandise/views.py
@@ -11,13 +11,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 def merchandise_list_view(request):                   #http://127.0.0.1:8000/merchandise_list_view/
     queryset = Merchandise.objects.all()
     context = {'object_list': queryset}
-    return render(request, "index.html", context)
+    return render(request, "list.html", context)
 
 
 
 def merchandise_detail_view(request, id, *args, **kwargs):    #http://127.0.0.1:8000/merchandise_detail_view/1/
 	instance = get_object_or_404(Merchandise, id=id)
 	context = {'object': instance}
-	return render(request, 'index.html', context)
+	return render(request, 'detail.html', context)
 
 
